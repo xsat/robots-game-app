47833c7d1cacbdadbc6c30509be7a5d4880cacfe
xsat <yurii.antonchyk@gmail.com>
2020-02-12 22:32:20 +0200

    Removed debug and added integration with heroku

--- src/game/constants.js
+++ src/game/constants.js
@@ -1,5 +1,3 @@
-export const GAME_API_URL = process.env.GAME_API_URL ?? "http://localhost:8089";
+export const GAME_API_URL = process.env.GAME_API_URL ?? "https://robots-game-api.herokuapp.com";
 export const TOKEN_KEY = "token";
 export const GAME_KEY = "game";
-
-console.log(process.env, GAME_API_URL);
